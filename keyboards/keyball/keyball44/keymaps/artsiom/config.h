232180f5aea8653ebaa37abfaff7463b98c657ad
Artem Ilyin <artsiom.ilyin@mapbox.com>
2025-06-23 08:06:21 +0200

    adjust keymap for trackball usage

---
+++ keyboards/keyball/keyball44/keymaps/artsiom/config.h
@@ -0,0 +1,88 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#endif
+
+#define KEYBALL_SCROLL_DIV_DEFAULT 6
+#define KEYBALL_CPI_DEFAULT 1000
+#define KEYBALL_CPI_SNIPE_MODE 500
+
+#define TAP_CODE_DELAY 5
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 3
+
+#undef LAYER_STATE_8BIT
+// Increase layer limit (default is 8 layers)
+#define LAYER_STATE_16BIT
+
+
+// custom code 
+#define U_NP KC_NO // key is not present
+#define U_NA KC_NO // present but not available for use
+#define U_NU KC_NO // available but not used
+
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define RAW_EPSIZE 32
+
+#ifdef RGB_MATRIX_ENABLE
+#define RGB_MATRIX_DEFAULT_HUE 72
+#define RGB_MATRIX_HUE_STEP 8
+
+#endif
---
+++ keyboards/keyball/keyball44/keymaps/artsiom/definitions.h
@@ -0,0 +1,83 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include "quantum.h"
+#include "lib/oledkit/oledkit.h"
+#include "lib/keyball/keyball.h"
+// #include "send_string_keycodes.h"
+
+enum LAYERS  {
+    U_BASE,
+    U_EXTRA,
+    U_TAP,
+    U_BUTTON,
+    U_NAV,
+    U_MOUSE,
+    U_MEDIA,
+    U_NUM,
+    U_SYM,
+    U_FUN
+};
+
+enum custom_keycodes {
+    // Add your layer names here if they are in a different enum
+    // _BASE, _NAV, etc...
+
+    // Universal editing keys
+    U_CUT = SAFE_RANGE,
+    U_CPY,
+    U_PST,
+    U_UND,
+    U_RDO,
+
+    // language things
+    U_DOT,
+    U_COMM,
+    U_YO,
+    LANG_SW,    
+
+    // OS Toggle Key
+    OS_SWAP,
+
+    SNIPE_MODE_TO
+};
+
+enum tapDance {
+    // Tap Dance
+    U_TD_BOOT,
+    // Layers
+    U_TD_U_BASE,
+    U_TD_U_EXTRA,
+    U_TD_U_TAP,
+    U_TD_U_BUTTON,
+    U_TD_U_NAV,
+    U_TD_U_MOUSE,
+    U_TD_U_MEDIA,
+    U_TD_U_NUM,
+    U_TD_U_SYM,
+    U_TD_U_FUN
+};
+
+//
+// --- Global Variable Declarations ---
+// We declare the variables here with 'extern' to tell other files
+// that they exist and are defined elsewhere (in logic.c).
+//
+
+extern bool is_mac;
+extern bool is_russian;
+extern enum LAYERS active_default_layer;
+#define TAP_DANCE_ACTIONS_SIZE 11 
+extern tap_dance_action_t tap_dance_actions[TAP_DANCE_ACTIONS_SIZE];
+
+// #define KEY_OVERRIDE_COUNT 1
+// extern const key_override_t *key_overrides[KEY_OVERRIDE_COUNT + 1];
+
+#define TD_SET_LAYER(layer) \
+  { .fn = { NULL, set_default_layer_td, NULL }, .user_data = (void*)(uintptr_t)layer }
+
+void set_default_layer_td(tap_dance_state_t *state, void *user_data);
+void u_td_fn_boot(tap_dance_state_t *state, void *user_data);
+
+
+#endif // DEFINITIONS_H
